c703a0aa1502b5d09a6cee2ec4f385568b73932c
serhat <serhat@poilabs.com>
2021-02-16 16:10:12 +0300

    typo

--- app/validators/index.js
+++ app/validators/index.js
@@ -4,10 +4,10 @@ module.exports = {
             if (!username || !password) {
                 throw Error("No username or password");
             }
-            if (username.length < 6 || password.length < 6) {
+            if (username.length <= 6 || password.length <= 6) {
                 throw Error("username and password length should be > 6");
             }
-            if (username.length > 32 || password.length > 32) {
+            if (username.length >= 32 || password.length >= 32) {
                 throw Error("username and password length should be < 32");
             }
             return true;
